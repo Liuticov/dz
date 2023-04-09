28bef1278aa70e893b1f94735172f93728752f03
Bhaltair <gilliarool@mail.ru>
2023-04-09 15:03:09 +0300

    Тренировочное задание2

---
+++ Task #1/Task #1.1.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("min");
+int min = ReadInt();
+
+System.Console.WriteLine("max");
+int max = ReadInt();
+
+int number = GetRandomNumber(min, max);
+
+if (GetFirstDigit(number) > GetSecondDigit(number))
+{
+    System.Console.WriteLine("first");
+}else{
+    System.Console.WriteLine("second");
+}
+
+int GetRandomNumber(int min, int max)
+{
+    return new Random().Next(min, max + 1);
+}
+
+int GetFirstDigit(int number)
+{
+    return number / 10;
+}
+
+int GetSecondDigit(int number)
+{
+    return number % 10;
+}
+
+int ReadInt()
+{
+    return int.Parse(Console.ReadLine());
+}
