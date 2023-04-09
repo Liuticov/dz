2e5cf0debf7b1f7ae04f0bc58e0f60dbc28b961a
Bhaltair <gilliarool@mail.ru>
2023-04-09 14:51:07 +0300

    Тренировочное задание

---
+++ Task #1/Task #1.cs
@@ -0,0 +1,10 @@
+﻿
+
+int number = new Random().Next(10, 100);
+int first = number / 10;
+int second = number % 10;
+Console.WriteLine("Случайное двузначное число: " + number);
+if (first > second)
+    Console.Write("Большее число из диапозона: " + first);
+else
+    Console.Write("Большее число из диапозона: " + second);
