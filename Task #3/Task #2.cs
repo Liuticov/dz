7aa9885ad1a7ac1f3e3964ee58e871541e455d68
Bhaltair <gilliarool@mail.ru>
2023-04-09 16:58:28 +0300

    Сделанное на Семинаре

---
+++ Task #3/Task #2.cs
@@ -0,0 +1,8 @@
+﻿
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Случайное трехзначное число: "+number);
+int first = number / 100;
+int third = number % 10;
+Console.Write("Число без второго: "+first+third);
+
---
+++ Task #5.1/Task #4.1.cs
@@ -0,0 +1,6 @@
+﻿// Вычислить площадь круга
+
+Console.WriteLine("Введите радиус круга: ");
+int radius = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Площадь круга = " + Math.PI * Math.Pow(radius, 2));
