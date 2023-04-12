010086df0311d598ae7f783a62d891c568562636
Bhaltair <gilliarool@mail.ru>
2023-04-12 21:59:03 +0300

    01.Rectile

---
+++ Homework/Task #1/RectileMethod.cs
@@ -0,0 +1,43 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число : ");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+
+for (b = 0; (a < 100 || a > 999) && (b <= 9); b += 1)
+{
+    Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число ПОЖАЛУЙСТА : ");
+    a = int.Parse(Console.ReadLine());
+}
+if (b <= 9)
+{
+    int first = a / 10;
+    int second = first % 10;
+    Console.Write("Вторая цифра в введенном числе: " + second);
+}
+
+else
+{
+    if (b == 10)
+    {
+        if (a < 100 || a > 999)
+        {
+            Console.WriteLine("Я СДАЮСЬ: ");
+        }
+        else
+        {
+            int first = a / 10;
+            int second = first % 10;
+            Console.Write("Вторая цифра в введенном числе: " + second);
+
+        }
+
+    }
+
+
+}
+
