b46ebaef98e71ddc1848b2b746997e43d595e9be
Bhaltair <gilliarool@mail.ru>
2023-04-12 22:22:28 +0300

    001.Task#1.End

---
+++ Homework/Task #1.1/Task#1 End.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+bool CheckNumber(int number)
+{
+    if (number >= 100 && number <= 999)
+    {
+        int first = number / 10;
+        int second = first % 10;
+        Console.WriteLine("Вторая цифра в введенном числе: " + second);
+        return true;
+    }
+    else
+    { return false; }
+}
+
+Console.WriteLine("Введите трехзначное число : ");
+int a = int.Parse(Console.ReadLine());
+bool result = CheckNumber(a);
+Console.WriteLine(result);
