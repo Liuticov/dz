b3bfbb30d48340821ec04a88db074011c399d463
Bhaltair <gilliarool@mail.ru>
2023-04-12 23:32:10 +0300

    03.Тask#3.Еnd.

---
+++ Homework/Task #3/Task#3 End.cs
@@ -0,0 +1,38 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+string CheckWeekdays(int weekdays)
+{
+    if (weekdays == 6 || weekdays == 7)
+    {
+        return "Да, это выходной";
+    }
+    else
+    { 
+        return "Нет, это не выходной";
+    }
+}
+bool CheckNumber(int number)
+{
+    if (number <= 7 && number > 0 )
+    {
+        return true;
+    }
+    else
+    { return false; }
+}
+
+Console.Write("Введите число: ");
+int days_in_week = int.Parse(Console.ReadLine());
+
+if (CheckNumber(days_in_week))
+{
+    Console.WriteLine(CheckWeekdays(days_in_week));
+}
+else
+    Console.WriteLine("Неверно введенное число ");
+
+
